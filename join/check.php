01c9c4ce4a79990328bec6c64243539595b03d7a
Sihoo-milk <>
2020-06-05 11:14:33 +0900

    登録確認画面を修正

--- join/check.php
+++ join/check.php
@@ -3,11 +3,24 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>登録完了</title>
+  <title>登録確認</title>
   <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-  <p>ユーザ登録が完了しました</p>
-  <p><a href="../">ログインする</a></p>
+  <form action="" method="post">
+    <dl>
+      <dt>ニックネーム</dt>
+      <dd></dd>
+      <dt>メールアドレス</dt>
+      <dd></dd>
+      <dt>パスワード</dt>
+      <dd>【表示されません】</dd>
+      <dt>写真など</dt>
+      <dd></dd>
+    </dl>
+    <div>
+      <a href="index.php?action=rewrite">&laquo;&nbsp;書き直す</a> | <input type="submit" value="登録する">
+    </div>
+  </form>
 </body>
 </html>
